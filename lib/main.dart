41907869bd3c05fa623b96d52bd0625fadc26709
Pawin Nakvisai <pawinnak2@gmail.com>
2023-11-30 00:53:37 +0700

    [ADD] Real-time video object detection

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,15 @@ import 'package:camera/camera.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:paystation_frontend/page/camera_page.dart';
 import 'package:flutter/material.dart';
+import 'package:pytorch_lite/pytorch_lite.dart';
 
 late List<CameraDescription> cameras;
+late ModelObjectDetection objectModel;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   cameras = await availableCameras();
+
   runApp(MaterialApp(
     title: 'PayStation',
     home: CameraPage(cameras: cameras),
